0d656557f12e775cdb7dd015a1972b07c021ef23
Brian Bothwell <sysrage@gmail.com>
2022-12-02 13:52:42 -0500

    day 2 part 2

--- day2.mjs
+++ day2.mjs
@@ -5,6 +5,10 @@ import readline from 'node:readline';
 // B/Y = Paper = 2 Points
 // C/Z = Scissors = 3 Points
 
+// X = Lose
+// Y = Draw
+// Z = Win
+
 const win = {
     'Y': 'A',
     'Z': 'B',
@@ -14,13 +18,22 @@ const win = {
 const lose = {
     'Z': 'A',
     'X': 'B',
-    'Y': 'C'
+    'Y': 'C',
 };
 
 const points = {
+    'A': 1,
+    'B': 2,
+    'C': 3,
     'X': 1,
     'Y': 2,
     'Z': 3,
+};
+
+const moveMap = {
+    'X': 'A',
+    'Y': 'B',
+    'Z': 'C',
 };
 
 console.log('AOC 2022 - Day #2');
@@ -29,18 +42,38 @@ const fileStream = fs.createReadStream('./input/day2.txt');
 const rl = readline.createInterface({ input: fileStream });
 
 let score = 0;
+let score2 = 0;
 for await (const line of rl) {
     const [ oponent, player ] = line.split(' ');
+
+    // Part 1
     if (oponent === win[player]) {
-    score += 6 + points[player];
+        // Player Wins
+        score += 6 + points[player];
     } else if (oponent === lose[player]) {
-    score += points[player];
+        // Oponent Wins
+        score += points[player];
     } else {
-    score += 3 + points[player];
+        // Draw
+        score += 3 + points[player];
+    }
+
+    // Part 2
+    if (player === 'X') {
+        // need to lose
+        const playerMove = moveMap[Object.entries(lose).filter(([key, val]) => val === oponent)[0][0]];
+        score2 += points[playerMove];
+    } else if (player === 'Y') {
+        // need to tie
+        const playerMove = oponent;
+        score2 += 3 + points[playerMove];
+    } else {
+        // need to win
+        const playerMove = moveMap[Object.entries(win).filter(([key, val]) => val === oponent)[0][0]]
+        score2 += 6 + points[playerMove];
     }
 }
 
 console.log(`Part 1: ${score}`);
 
-
-// console.log(`Part 2: ${topThree}`);
+console.log(`Part 2: ${score2}`);
